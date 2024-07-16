6389eed48232049c68041f2a041104a8defdb6f7
sahatovbatyr <sahatovbatyr@gmail.com>
2024-07-16 08:51:40 +0300

    added get-by-id and create methods

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,16 +1,45 @@
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from app.database.db_config import get_db
 from app.models.model import User
-from app.schemas.user_scheme import UserCreateDto
+from app.repositories.user_reopsitory import UserRepository
+from app.schemas.user_scheme import UserCreateDto, UserShowDto
+from app.services.user_role_service import UserRoleService
 
-def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
 
-def create_user(db: Session, user: UserCreateDto):
-    db_user = User(username=user.username, password=user.password)
+class UserService:
 
-    db_user = User()
+    def __init__(self ):
+        self.session = next(get_db())
+        self.user_repository = UserRepository(self.session)
+        self.user_role_service = UserRoleService( self.session )
 
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    def get_by_id(self, user_id: int):
+        # user: User = self.session.query(User).filter(User.id == user_id).first()
+        user: User = self.user_repository.get_by_id(user_id)
+        return UserShowDto.model_validate(user)
+
+    def create(self, userCreateDto: UserCreateDto) -> User:
+
+        if userCreateDto.password != userCreateDto.password_confirmation:
+            raise HTTPException(400, "Password and confirmation not match!")
+
+        if len(userCreateDto.roles) == 0:
+            raise HTTPException(400, "User must have at least one role.")
+
+        user_is_exists = self.user_repository.is_username_exist(userCreateDto.username)
+
+        if user_is_exists:
+            raise HTTPException(400, f"User with username: {userCreateDto.username} already exists.")
+
+        roles = self.user_role_service.get_by_id_list(userCreateDto.roles)
+
+        user: User = User()
+        user.roles = roles
+        user.password = userCreateDto.password
+        user.username = userCreateDto.username
+        user = self.user_repository.create( user )
+        return user
+
+
