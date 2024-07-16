103d577af03aa14e41dcbd002ed104195377958d
sahatovbatyr <sahatovbatyr@gmail.com>
2024-07-16 08:58:50 +0300

    fixed session and used repository

--- app/services/user_role_service.py
+++ app/services/user_role_service.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
@@ -9,18 +11,20 @@ from app.schemas.user_role_scheme import UserRoleCreateDto
 
 class UserRoleService:
 
-    def __init__(self, db: Session = Depends(get_db)):
-        self.db = db
-        self.user_role_repository = UserRoleRepository(db)
+    def __init__(self, session:Session = None):
+        self.session =  session if session is not None else next(get_db())
+        self.user_role_repository = UserRoleRepository(self.session)
 
 
-    def get_by_id(self, role_id: int):
-        self.user_role_repository.get_by_id( role_id)
+    def get_by_id(self, role_id: int) -> UserRole:
+        return self.user_role_repository.get_by_id( role_id)
+
+    def get_by_id_list(self, roles: List[UserRole]):
+        role_id_list = [ role.id for role in roles]
+        return self.user_role_repository.get_all_by_list_id( role_id_list)
 
     def create_user_role(self, role_dto: UserRoleCreateDto):
-        role = UserRole(role_name=role_dto.role_name)
-        self.user_role_repository.create(role)
+        role = self.user_role_repository.create(role_dto)
         return role
-
 
 
