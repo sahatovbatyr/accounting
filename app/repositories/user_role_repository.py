c54b3c22254ca926d3f95922679f6fc1e383796f
sahatovbatyr <sahatovbatyr@gmail.com>
2024-07-16 08:57:30 +0300

    fixed session and used repository

--- app/repositories/user_role_repository.py
+++ app/repositories/user_role_repository.py
@@ -1,3 +1,6 @@
+from typing import Set, List, Type
+from fastapi import HTTPException
+
 from sqlalchemy.orm import Session
 
 from app.models.model import UserRole
@@ -9,7 +12,7 @@ class UserRoleRepository:
         self.db = db
 
     def create(self, roleCreateDto: UserRoleCreateDto ) -> UserRole:
-        new_role = UserRole(None, roleCreateDto.role_name)
+        new_role = UserRole(role_name= roleCreateDto.role_name)
         self.db.add(new_role)
         self.db.commit()
         self.db.refresh(new_role)
@@ -17,7 +20,10 @@ class UserRoleRepository:
 
 
     def get_by_id(self, id: int ) -> UserRole:
-        return self.db.query(UserRole).filter(UserRole.id == id).first()
+        role = self.db.query(UserRole).filter(UserRole.id == id).first()
+        if role is None:
+            raise HTTPException(status_code=400, detail=f"Role not found by id: {id}")
+        return role
 
 
     def update(self, user_role: UserRoleDto ) -> UserRole:
@@ -26,4 +32,10 @@ class UserRoleRepository:
         self.db.commit()
         self.db.refresh(new_role)
         return new_role
+
+    def get_all_by_list_id(self, id_list: List[int] ) -> List[UserRole]:
+        roles: List[UserRole] = self.db.query(UserRole).filter(UserRole.id.in_(id_list)).all()
+        # converted_users: List[UserRole] = [UserRole(role.id, role.role_name) for role in roles_from_db]
+        return roles
+
 
