535a86e9c4cc9c73f5cb142a4c33b664b0b5ea67
sahatovbatyr <sahatovbatyr@gmail.com>
2024-07-16 08:55:51 +0300

    fixed associations

--- app/models/model.py
+++ app/models/model.py
@@ -3,18 +3,6 @@ from sqlalchemy.orm import relationship
 from app.database.db_config import Base
 
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
-    roles = relationship("UserRole", back_populates="users")
-
-class UserRole(Base):
-    __tablename__ = "user_roles"
-    id = Column(Integer, primary_key=True, index=True)
-    role_name = Column(String, unique=True, index=True)
-
 
 users_roles_association = Table(
     'users_roles',
@@ -22,6 +10,27 @@ users_roles_association = Table(
     Column('user_id', Integer, ForeignKey('users.id')),
     Column('role_id', Integer, ForeignKey('user_roles.id'))
 )
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    roles = relationship("UserRole", secondary=users_roles_association)
+
+    class Config:
+        from_attributes = True
+
+
+class UserRole(Base):
+    __tablename__ = "user_roles"
+    id = Column(Integer, primary_key=True, index=True)
+    role_name = Column(String, unique=True, index=True)
+
+    class Config:
+        from_attributes = True
+
+
+
 
 
 class Task(Base):
@@ -33,7 +42,3 @@ class Task(Base):
 
     author = relationship("User", foreign_keys=[author_id])
     assigned_to = relationship("User", foreign_keys=[assigned_to_id])
-
-
-
-
