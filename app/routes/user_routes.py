49e581a51966c25802f53ff3ca040f1b01ddbfc4
sahatovbatyr <sahatovbatyr@gmail.com>
2024-07-17 19:31:39 +0300

    added change_password  update_roles end points

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app.models.model import User
-from app.schemas.user_scheme import UserShowDto, UserCreateDto
+from app.models.model import User, UserRole
+from app.schemas.user_scheme import UserShowDto, UserCreateDto, UserChangePasswordDto, UserChangeRoleDto
 # from app.services.user_service import cr
 from app.database.db_config import SessionLocal
 from app.services.user_service import UserService
@@ -13,7 +13,7 @@ from app.services.user_service import UserService
 
 class UserRouter:
     def __init__(self):
-        self.router = APIRouter();
+        self.router = APIRouter()
         self.user_service = UserService()
 
         @self.router.post("/create/")
@@ -25,6 +25,29 @@ class UserRouter:
         def get_by_id(id: int):
             user: User = self.user_service.get_by_id(id)
             return user
+
+        @self.router.post("/change-password/")
+        def user_change_password( userChangePasswordDto: UserChangePasswordDto):
+
+            if userChangePasswordDto.new_password != userChangePasswordDto.new_password_confirmation:
+                raise HTTPException(400, "Passwords not match")
+
+            user: User = User()
+            user.id = userChangePasswordDto.id
+            user.username = userChangePasswordDto.username
+            user.password = userChangePasswordDto.password
+            new_password = userChangePasswordDto.new_password
+            user = self.user_service.change_password(user, new_password)
+            return UserShowDto.model_validate( user)
+
+        @self.router.post("/update-roles/")
+        def user_update_roles(userChangeRoleDto: UserChangeRoleDto):
+            user: User = User()
+            user.id = userChangeRoleDto.id
+            user.username = userChangeRoleDto.username
+            roles = [ UserRole( id = r.id, role_name= r.role_name)  for r in userChangeRoleDto.roles ]
+            user = self.user_service.update_roles(user, roles )
+            return UserShowDto.model_validate(user)
 
 
 
