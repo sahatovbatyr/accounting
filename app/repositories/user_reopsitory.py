868f53015e3da1f318b6f1558334dc6704cce0a8
sahatovbatyr <sahatovbatyr@gmail.com>
2024-07-17 19:30:44 +0300

    added change_password  update_roles methods

--- app/repositories/user_reopsitory.py
+++ app/repositories/user_reopsitory.py
@@ -1,6 +1,8 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
-from app.models.model import User
+from app.models.model import User, UserRole
 from app.repositories.user_role_repository import UserRoleRepository
 from app.schemas.user_scheme import UserCreateDto
 
@@ -30,4 +32,23 @@ class UserRepository:
     def is_username_exist(self, username: str) -> bool:
         user: User = self.session.query(User).filter(User.username == username).first()
         return True if user else False
+
+    def change_password(self, user: User, new_password: str) -> User:
+        user_db: User = self.session.query(User).filter(User.id == user.id).first()
+        user_db.password = new_password
+        self.session.merge(user_db)
+        self.session.commit()
+        self.session.refresh(user_db)
+        return user_db
+
+
+    def update_roles(self, user: User, new_roles:List[UserRole]) -> User:
+        user_db: User = self.session.query(User).filter(User.id == user.id).first()
+        user_db.roles = new_roles
+        self.session.merge(user_db)
+        self.session.commit()
+        self.session.refresh(user_db)
+        return user_db
+
+
 
